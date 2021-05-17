bca73cf91767d8039fc3dfb609c4f0cf12fc5493
NORMAND <arthur.normand@capgemini.com>
2021-05-17 18:19:10 +0200

    removing iframe

--- app.js
+++ app.js
@@ -31,11 +31,8 @@ window.addEventListener("load", cameraStart, false);
       navigator.geolocation.watchPosition(function (position) {
         // Managing logging on the left of the screen
         var displayed_Logs = document.getElementById('logs');
-        var ar_Iframe = document.getElementById('arFrame');
-        const distance_Message = ar_Iframe.contentWindow.document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
         displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; 
-        latitude:${position.coords.latitude};
-        and ${distance_Message} away from you`;
+        latitude:${position.coords.latitude}`;
         // Managing form pre-filling with gps coordinates
         var lat = document.getElementById('lat');
         var long = document.getElementById('long');
