54bb52933d00c431a8bd73a337a1ab6928c1c15a
NORMAND <arthur.normand@capgemini.com>
2021-05-17 18:04:50 +0200

    Adding basic logic from arjs project

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const log = console.log;
+
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "environment" }, audio: false };// Define constants
 const cameraView = document.querySelector("#camera--view"),
@@ -23,3 +25,22 @@ cameraTrigger.onclick = function() {
     cameraOutput.classList.add("taken");
 };// Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
+  window.onload = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(function (position) {
+        // Managing logging on the left of the screen
+        var displayed_Logs = document.getElementById('logs');
+        var ar_Iframe = document.getElementById('arFrame');
+        const distance_Message = ar_Iframe.contentWindow.document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+        displayed_Logs.innerHTML = `longitude:${position.coords.longitude}; 
+        latitude:${position.coords.latitude};
+        and ${distance_Message} away from you`;
+        // Managing form pre-filling with gps coordinates
+        var lat = document.getElementById('lat');
+        var long = document.getElementById('long');
+        lat.value=position.coords.latitude;
+        long.value=position.coords.longitude;
+      });
+    }
+};
