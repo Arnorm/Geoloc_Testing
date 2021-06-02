e7adaec001cbd30b469d4a9bee82890ce4636002
NORMAND <arthur.normand@capgemini.com>
2021-06-02 14:36:32 +0200

    testing fiddle

--- basicTree.js
+++ basicTree.js
@@ -4,24 +4,30 @@
 
 import * as THREE from './threeJs/build/three.module.js';
 
+// ------------------------------------------------
+// BASIC SETUP
+// ------------------------------------------------
+
 // Create an empty scene
 var scene = new THREE.Scene();
+
 // Create a basic perspective camera
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
+
 // Create a renderer with Antialiasing
-var renderer = new THREE.WebGLRenderer({antialias:true});
+var renderer = new THREE.WebGLRenderer({
+antialias: true
+});
+
 // Configure renderer clear color
 renderer.setClearColor("#000000");
+
 // Configure renderer size
-renderer.setSize( window.innerWidth, window.innerHeight );
-
-
-// Change the entry point
-container = document.getElementById('right_View');
-//document.body.appendChild(container);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Append Renderer to DOM
+container = document.getElementById('right_View');
 container.appendChild(renderer.domElement);
 
 // ------------------------------------------------
@@ -29,22 +35,24 @@ container.appendChild(renderer.domElement);
 // ------------------------------------------------
 
 // Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
-var cube = new THREE.Mesh( geometry, material );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({
+color: "#433F81"
+});
+var cube = new THREE.Mesh(geometry, material);
 
 // Add cube to Scene
-scene.add( cube );
+scene.add(cube);
 
 // Render Loop
-var render = function () {
-  requestAnimationFrame( render );
+var render = function() {
+requestAnimationFrame(render);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
 
-  // Render the scene
-  renderer.render(scene, camera);
+// Render the scene
+renderer.render(scene, camera);
 };
 
 render();
