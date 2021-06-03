b8064a3179c0ad47f4b2bd12ab7eef919f5173a0
arnorm <arthur.normand@capgemini.com>
2021-06-03 14:59:15 +0200

    changed button renderer

--- basicTree.js
+++ basicTree.js
@@ -19,6 +19,7 @@ function init() {
 	// Change the entry point
     container = document.getElementById('right_View');
 	document.body.appendChild(container);
+    
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
 	const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
@@ -29,7 +30,7 @@ function init() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.xr.enabled = true;
 	container.appendChild(renderer.domElement);
-	document.body.appendChild(ARButton.createButton(renderer, {requiredFeatures: ['hit-test']}));
+	container.appendChild(ARButton.createButton(renderer, {requiredFeatures: ['hit-test']}));
 	const geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.2, 32).translate(0, 0.1, 0);
 
 	function onSelect() {
