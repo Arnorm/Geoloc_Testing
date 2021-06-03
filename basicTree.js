7e582fa1f6e8fc6e5a90bc872745bd3686bae514
arnorm <arthur.normand@capgemini.com>
2021-06-03 10:33:30 +0200

    treeJs hittest

--- basicTree.js
+++ basicTree.js
@@ -5,57 +5,91 @@
 import * as THREE from './threeJs/build/three.module.js';
 import { ARButton } from './threeJs/examples/jsm/webxr/ARButton.js';
 
-// ------------------------------------------------
-// BASIC SETUP
-// ------------------------------------------------
+let container;
+let camera, scene, renderer;
+let controller;
+let reticle;
+let hitTestSource = null;
+let hitTestSourceRequested = false;
 
-var container;
+init();
+animate();
 
-// Create an empty scene
-var scene = new THREE.Scene();
+function init() {
+	// Change the entry point
+    container = document.getElementById('right_View');
+	document.body.appendChild(container);
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+	const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+	light.position.set(0.5, 1, 0.25);
+	scene.add(light);
+	renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.xr.enabled = true;
+	container.appendChild(renderer.domElement);
+	document.body.appendChild(ARButton.createButton(renderer, {requiredFeatures: ['hit-test']}));
+	const geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.2, 32).translate(0, 0.1, 0);
 
-// Create a basic perspective camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 4;
+	function onSelect() {
+		if (reticle.visible) {
+			const material = new THREE.MeshPhongMaterial({color: 0xffffff * Math.random()});
+			const mesh = new THREE.Mesh(geometry, material);
+			mesh.position.setFromMatrixPosition(reticle.matrix);
+			mesh.scale.y = Math.random() * 2 + 1;
+			scene.add(mesh);
+		}
+	}
+	controller = renderer.xr.getController(0);
+	controller.addEventListener('select', onSelect);
+	scene.add(controller);
+	reticle = new THREE.Mesh(
+		new THREE.RingGeometry(0.15, 0.2, 32).rotateX(- Math.PI / 2),
+		new THREE.MeshBasicMaterial()
+	);
+	reticle.matrixAutoUpdate = false;
+	reticle.visible = false;
+	scene.add(reticle);
+	window.addEventListener('resize', onWindowResize);
+}
 
-// Create a renderer with Antialiasing
-var renderer = new THREE.WebGLRenderer({
-antialias: true
-});
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
-// Configure renderer clear color
-renderer.setClearColor("#000000");
+function animate() {
+	renderer.setAnimationLoop(render);
+}
 
-// Configure renderer size
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Append Renderer to DOM
-container = document.getElementById('right_View');
-container.appendChild(renderer.domElement);
-
-// ------------------------------------------------
-// FUN STARTS HERE
-// ------------------------------------------------
-
-// Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({
-color: "#433F81"
-});
-var cube = new THREE.Mesh(geometry, material);
-
-// Add cube to Scene
-scene.add(cube);
-
-// Render Loop
-var render = function() {
-requestAnimationFrame(render);
-
-cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
-
-// Render the scene
-renderer.render(scene, camera);
-};
-
-render();
+function render(timestamp, frame) {
+	if (frame) {
+		const referenceSpace = renderer.xr.getReferenceSpace();
+		const session = renderer.xr.getSession();
+		if (hitTestSourceRequested === false) {
+			session.requestReferenceSpace('viewer').then(function(referenceSpace) {
+				session.requestHitTestSource({space: referenceSpace}).then(function(source) {
+					hitTestSource = source;
+				});
+			});
+			session.addEventListener('end', function() {
+				hitTestSourceRequested = false;
+				hitTestSource = null;
+			});
+			hitTestSourceRequested = true;
+		}
+		if (hitTestSource) {
+			const hitTestResults = frame.getHitTestResults(hitTestSource);
+			if (hitTestResults.length) {
+				const hit = hitTestResults[0];
+				reticle.visible = true;
+				reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
+			} else {
+				reticle.visible = false;
+			}
+		}
+	}
+	renderer.render(scene, camera);
+}
