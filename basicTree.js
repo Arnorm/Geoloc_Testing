4cc5f6e751e56e0c32466701f9cbf27defae8d0b
arnorm <arthur.normand@capgemini.com>
2021-06-11 15:17:14 +0200

    going back

--- basicTree.js
+++ basicTree.js
@@ -1,12 +1,12 @@
 // Module is directly included in the project, we might want to change this
 import * as THREE from './threeJs/build/three.module.js';
 import {target_Long, target_Lat, angle_Treshold, is_IOS, xr_Button, info} from './const.js';
-//import {calc_Crow_Distance, bearing} from './auxiliaries.js';
+import {calc_Crow_Distance, bearing} from './auxiliaries.js';
 
 // Variables for sensors
 let is_Fullscreen_Active = false; // boolean needs to be removed later
 let compass = null;
-let min_Angle = 0;
+//let min_Angle = 0;
 var bearing_Device_Target = 0; // Angles declared as globals for now
 let distance_Device_Target = null;
 
@@ -207,7 +207,7 @@ function on_XR_Frame(t, frame) {
     gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer);
     // render the scene
     renderer.render(scene, camera);
-    handler_Display();
+    //handler_Display();
 }
 
 // Only for IOS, not tested YET
@@ -230,7 +230,22 @@ function handler_Orientation(e) {
     compass = e.webkitCompassHeading || Math.abs(e.alpha - 360); // not always defined otherwise
     var delta_Angle = bearing_Device_Target - compass;
     var abs_Delta_Angle = ((delta_Angle % 360) + 360) % 360; //Js % is not mod (see doc for more info)
-    min_Angle = Math.min(360 - abs_Delta_Angle, abs_Delta_Angle);
+    var min_Angle = Math.min(360 - abs_Delta_Angle, abs_Delta_Angle);
+    if (is_Fullscreen_Active==true) {
+        if(min_Angle<angle_Treshold){
+            if (reticle.visible && object_Placed<1) {
+                object_Placed = object_Placed + 1;
+                place_Object();
+            }
+            if (object_Placed<1) {
+                visual_Display.innerHTML = `Please move the reticle to a plane surface so that the object can be rendered !`;
+            }
+            visual_Display.innerHTML = `You found it ! Congratulations`;
+        }
+        else{
+            visual_Display.innerHTML = `Try to reduce the angle : ${min_Angle.toFixed(0)}`;
+        }
+    }
 }
 
 // Handles location sensor
@@ -271,74 +286,3 @@ function handler_Display() {
 
 // Triggers everything
 check_XR();
-
-// Auxiliaries
-
-//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
-function calc_Crow_Distance(start_Lat, start_Long, dest_Lat, dest_Long) 
-{
-    var R = 6371; // km
-    var delta_Lat = to_Radians(dest_Lat-start_Lat);
-    var delta_Long = to_Radians(dest_Long-start_Long);
-    var start_Lat = to_Radians(start_Lat);
-    var dest_Lat = to_Radians(dest_Lat);
-    var a = Math.sin(delta_Lat/2) * Math.sin(delta_Lat/2) +
-        Math.sin(delta_Long/2) * Math.sin(delta_Long/2) * Math.cos(start_Lat) * Math.cos(dest_Lat); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c;
-    return d;
-}
-
-// Converts from degrees to radians.
-function to_Radians(degrees) {
-    return degrees * Math.PI / 180;
-  };
-   
-// Converts from radians to degrees.
-function to_Degrees(radians) {
-    return radians * 180 / Math.PI;
-}
-  
-// Bearing formula, between two 2D points, clockwise angle between north and (start,dest)
-function bearing(start_Lat, start_Long, dest_Lat, dest_Long){
-    start_Lat = to_Radians(start_Lat);
-    start_Long = to_Radians(start_Long);
-    dest_Lat = to_Radians(dest_Lat);
-    dest_Long = to_Radians(dest_Long);
-    const y = Math.sin(dest_Long - start_Long) * Math.cos(dest_Lat);
-    const x = Math.cos(start_Lat) * Math.sin(dest_Lat) -
-          Math.sin(start_Lat) * Math.cos(dest_Lat) * Math.cos(dest_Long - start_Long);
-    var bearing_ = Math.atan2(y, x);
-    bearing_ = to_Degrees(bearing_);
-    return (bearing_ + 360) % 360;
-}
-
-// Compass heading (not used at the moment)
-function compass_Heading(alpha, beta, gamma) {
-    // Convert degrees to radians
-    var alpha_Rad = alpha * (Math.PI / 180);
-    var beta_Rad = beta * (Math.PI / 180);
-    var gamma_Rad = gamma * (Math.PI / 180);
-    // Calculate equation components
-    var cA = Math.cos(alpha_Rad);
-    var sA = Math.sin(alpha_Rad);
-    var cB = Math.cos(beta_Rad);
-    var sB = Math.sin(beta_Rad);
-    var cG = Math.cos(gamma_Rad);
-    var sG = Math.sin(gamma_Rad);
-    // Calculate A, B, C rotation components
-    var rA = - cA * sG - sA * sB * cG;
-    var rB = - sA * sG + cA * sB * cG;
-    //var rC = - cB * cG;
-    // Calculate compass heading
-    var compass_Heading = Math.atan(rA / rB);
-    // Convert from half unit circle to whole unit circle
-    if(rB < 0) {
-      compass_Heading += Math.PI;
-    }else if(rA < 0) {
-      compass_Heading += 2 * Math.PI;
-    }
-    // Convert radians to degrees
-    compass_Heading *= 180 / Math.PI;
-    return compass_Heading;
-}
